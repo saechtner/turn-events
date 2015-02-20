9826528da9032cd12c8de241a0c7f323938b7db4
Nico <nico.aechtner@student.hpi.uni-potsdam.de>
2015-02-21 00:22:39 +0100

    - added comments

--- Turnauswertung/src/Main.java
+++ Turnauswertung/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        // test
         NA_DB_Connector con = new NA_DB_Connector("","","");
 
         NA_MainView window = new NA_MainView();
