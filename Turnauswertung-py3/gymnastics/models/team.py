a8ecfaadf54483089619149dfd179b47d0b3bf61
Nico <nico.aechtner@student.hpi.uni-potsdam.de>
2015-03-15 15:11:47 +0100

    >

--- Turnauswertung-py3/gymnastics/models/team.py
+++ Turnauswertung-py3/gymnastics/models/team.py
@@ -1,9 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from gymnastics.models.stream import Stream
 
 class Team(models.Model):
   
     name = models.CharField(max_length=50, null=False)
+
+    stream = models.ForeignKey(Stream, null=False)
     
     def __str__(self):
         return self.name
