e10b8558f28071ebfb4035adc71a2f7bdd768c61
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-03-15 18:11:19 +0100

    changes to models

--- Turnauswertung-py3/gymnastics/models/athlete.py
+++ Turnauswertung-py3/gymnastics/models/athlete.py
@@ -1,5 +1,5 @@
-from django.core.urlresolvers import reverse
 from django.db import models
+
 from gymnastics.models.club import Club
 from gymnastics.models.squad import Squad
 from gymnastics.models.stream import Stream
@@ -18,6 +18,9 @@ class Athlete(models.Model):
     team = models.ForeignKey(Team, null=True, blank=True)
 
 
+    class Meta:
+        db_table = 'gymnastics_athletes'
+
     def __str__(self):
         return self.first_name + " " + self.last_name
 
@@ -26,6 +29,3 @@ class Athlete(models.Model):
             return 'female'
         else :
             return 'male'
-
-    class Meta:
-        db_table = 'gymnastics_athletes'
--- Turnauswertung-py3/gymnastics/models/club.py
+++ Turnauswertung-py3/gymnastics/models/club.py
@@ -1,13 +1,22 @@
-from django.core.urlresolvers import reverse
 from django.db import models
+
+# from gymnastics.models.athlete import Athlete
+# from gymnastics.models.club import Club
+# from gymnastics.models.squad import Squad
+# from gymnastics.models.stream import Stream
+# from gymnastics.models.team import Team
+
 
 class Club(models.Model):
   
     name = models.CharField(max_length=50, null=False)
+    
 
+    class Meta:
+        db_table = 'gymnastics_clubs'
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        db_table = 'gymnastics_clubs'
+    def authors(self):
+        return Athlete.objects.filter(club = self)
