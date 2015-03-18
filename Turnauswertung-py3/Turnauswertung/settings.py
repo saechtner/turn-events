ac0627e5c81f98474913427701cef5e7f77c924b
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-03-18 15:23:23 +0100

    added plugin django-widget-tweaks

--- Turnauswertung-py3/Turnauswertung/settings.py
+++ Turnauswertung-py3/Turnauswertung/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gymnastics'
+
+    'widget_tweaks',
+
+    'gymnastics',
 )
 
 MIDDLEWARE_CLASSES = (
