cc5cba11fc3de3bb399f37933a3fcebebe2754ef
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-04-12 19:57:01 +0200

    - made localized links available

--- Turnauswertung-py3/Turnauswertung/urls.py
+++ Turnauswertung-py3/Turnauswertung/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'auswertung.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^gymnastics/', include('gymnastics.urls')), 
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns = i18n_patterns('',
+    url(r'^gymnastics/', include('gymnastics.urls')), 
 )
