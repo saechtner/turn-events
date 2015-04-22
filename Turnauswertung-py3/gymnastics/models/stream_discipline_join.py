816c0411a45dcc365316558b4e26a4093b819b82
Nico <nico.aechtner@student.hpi.uni-potsdam.de>
2015-04-22 17:03:25 +0200

    WIP streams.new

---
+++ Turnauswertung-py3/gymnastics/models/stream_discipline_join.py
@@ -0,0 +1,12 @@
+import operator
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class StreamDisciplineJoin(models.Model):
+    position = models.IntegerField(null=True)
+    stream = models.ForeignKey('Stream')
+    discipline = models.ForeignKey('Discipline')
+
+    class Meta:
+        db_table = 'gymnastics_stream_discipline_joins'
