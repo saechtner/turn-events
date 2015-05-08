da32977e593572aef66063d9c096f43a89d15fec
Nico Aechtner <Nico@Nicos-MBP.lan>
2015-05-08 18:05:22 +0200

    added contact name to address model

--- Turnauswertung-py3/gymnastics/models/address.py
+++ Turnauswertung-py3/gymnastics/models/address.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.utils.translation import ugettext_lazy
 
 class Address(models.Model):
-    street = models.TextField(ugettext_lazy('street'), max_length=128)
-    city = models.TextField(ugettext_lazy('city'), max_length=128)
-    province = models.TextField(ugettext_lazy('province'), max_length=128)
-    zip_code = models.TextField(ugettext_lazy('zip code'), max_length=10)
+    contact_name = models.CharField(ugettext_lazy('Contact name'), max_length=64)
+    street = models.CharField(ugettext_lazy('Street'), max_length=128)
+    city = models.CharField(ugettext_lazy('City'), max_length=128)
+    province = models.CharField(ugettext_lazy('Province'), max_length=128)
+    zip_code = models.CharField(ugettext_lazy('Zip code'), max_length=10)
