d4445bd4f6bde629d035e736e548a106d6df96e4
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-05-23 16:01:05 +0200

    Project cleanup

--- Turnauswertung/src/turnauswertung/ResetSettings.java
+++
@@ -1,9 +0,0 @@
-package turnauswertung;
-import turnauswertung.controller.JSONController;
-
-public class ResetSettings {
-	
-    public static void main(String[] args){
-        JSONController.resetJSONToDefault(JSONController.settingsPath());
-    }
-}
