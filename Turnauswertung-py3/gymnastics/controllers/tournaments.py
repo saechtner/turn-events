40497b6c492126082af9fc3bf12379af08a4a4aa
Sten Aechtner <sten.aechtner@student.hpi.uni-potsdam.de>
2015-05-23 17:01:42 +0200

    Project cleanup and small bugfix in tournaments.main

--- Turnauswertung-py3/gymnastics/controllers/tournaments.py
+++ Turnauswertung-py3/gymnastics/controllers/tournaments.py
@@ -11,19 +11,18 @@ from gymnastics.models import Club, Tournament
 from gymnastics.utils import pdf
 
 
-# TODO: find better solution to handle static constants such as this one
-main_tournament_name = 'KJSS 2015'
-
 def index(request):
     context = { 'tournaments': Tournament.objects.all().select_related('club')}
     return render(request, 'gymnastics/tournaments/index.html', context)
 
 def main(request):
+
+    # TODO: find better solution to handle static constants such as this one
+    main_tournament_name = 'KJSS 2015'
+    
     try:
         tournament = Tournament.objects.filter(name=main_tournament_name)[0]
-        context = { 'tournament': tournament }
-        # return render(request, 'gymnastics/tournaments/detail.html', context)
-        return redirect(reverse('tournaments.detail', kwargs={ 'id': tournament.id }))
+        return redirect(tournament.get_absolute_url())
     except:
         return redirect(reverse('tournaments.new'))
 
