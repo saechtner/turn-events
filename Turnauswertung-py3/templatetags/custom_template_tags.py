c6e72e30168be68f0a2ca0259257a7cfc00c18af
Sten Aechtner <sten.aechtner@esharesinc.com>
2017-01-08 18:03:43 -0800

    moved templatetags to root package

--- Turnauswertung-py3/templatetags/custom_template_tags.py
+++ Turnauswertung-py3/templatetags/custom_template_tags.py
@@ -3,21 +3,31 @@ from django.utils.translation import ugettext_lazy
 
 register = template.Library()
 
+
 @register.inclusion_tag('templatetags/nav_item.html')
 def render_nav_item(name, url_name='', url_id=None, active=False):
-    return {'name': ugettext_lazy(str(name)), 'url_name': url_name, 'url_id': url_id, 'active': active}
+    return {
+        'name': ugettext_lazy(str(name)),
+        'url_name': url_name,
+        'url_id': url_id,
+        'active': active
+    }
+
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key, '')
 
+
 @register.filter
 def get_index(list_, index):
     return list_[int(index)]
 
+
 @register.filter
 def get_final_total(athlete, discipline):
     return athlete.final_total(discipline)
+
 
 @register.filter
 def url_contains_domain(url, domain):
